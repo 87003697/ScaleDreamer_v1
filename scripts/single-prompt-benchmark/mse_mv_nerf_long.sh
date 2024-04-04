10347d36abe5cb031e79cc545939263b562b5941
theEricMa <zm2354@columbia.edu>
2024-04-04 11:07:30 +0000

    update

---
+++ scripts/single-prompt-benchmark/mse_mv_nerf_long.sh
@@ -0,0 +1,5 @@
+python launch.py \
+    --config configs/single-prompt_benchmark/mse_mv_nerf_long.yaml \
+    --train \
+    --gpu 7 \
+    system.prompt_processor.prompt=configs/single-prompt_benchmark/mse_mv_nerf_long.yaml
---
+++ scripts/single-prompt-benchmark/vsd_sd_nerf_long.sh
@@ -0,0 +1,5 @@
+python launch.py \
+    --config configs/single-prompt_benchmark/vsd_sd_nerf_long.yaml \
+    --train \
+    --gpu 7 \
+    system.prompt_processor.prompt=configs/single-prompt_benchmark/vsd_sd_nerf_long.yaml
