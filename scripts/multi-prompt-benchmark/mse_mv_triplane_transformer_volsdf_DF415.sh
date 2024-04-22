7229e049844ce52de36ed6766e1d78e37fa1c0f3
87003697 <870036797@qq.com>
2024-04-22 13:24:40 +0800

    update

--- scripts/multi-prompt-benchmark/mse_mv_triplane_transformer_volsdf_DF415.sh
+++ scripts/multi-prompt-benchmark/mse_mv_triplane_transformer_volsdf_DF415.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7  python launch.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  python launch.py \
     --config configs/multi-prompt_benchmark/mse_mv_triplane_transformer_volsdf_10k.yaml \
     --train \
     system.prompt_processor.prompt_library="dreamfusion_415_prompt_library"
