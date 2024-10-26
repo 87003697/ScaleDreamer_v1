687203314d46988e868f290e3a1deefbb0844977
87003697 <870036797@qq.com>
2024-10-26 20:39:44 +0800

    update

--- scripts/group_8-1/exp6_3DTopia__base2_step_4__asd_mv+rd+sd_volsdf+diffmc-0_hexa_16_vanilla_16_wobias_lora_prompt_v2.sh
+++ scripts/group_8-1/exp6_3DTopia__base2_step_4__asd_mv+rd+sd_volsdf+diffmc-0_hexa_16_vanilla_16_wobias_lora_prompt_v2.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7  python launch.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3  python launch.py \
     --config configs/group_8-1/exp6_3DTopia__base2_step_4__asd_mv+rd+sd_volsdf+diffmc-0_hexa_16_vanilla_16_wobias_lora_prompt_v2.yaml \
     --train \
     data.prompt_library="3DTopia_361k_prompt_library" \
