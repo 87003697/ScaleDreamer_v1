e415e6bbbc94c552b8a49701c582dddfce683530
87003697 <870036797@qq.com>
2024-12-06 19:37:53 +0800

    update

--- scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
+++ scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
@@ -11,9 +11,8 @@ torchrun \
     --master_port=12349 \
     --master_addr=$HOST_NODE_ADDR \
     launch.py \
-        --config configs/group_13/DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50.yaml \
+        --config configs/group_13/DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.yaml \
         --train \
-        trainer.num_nodes=$1 \
         data.prompt_library="DALLE_Midjourney_1313928_prompt_library" \
         data.condition_processor.cache_dir="/media/test/.threestudio_cache/text_embeddings_DALLE_Midjourney" \
         data.guidance_processor.cache_dir="/media/test/.threestudio_cache/text_embeddings_DALLE_Midjourney" 
