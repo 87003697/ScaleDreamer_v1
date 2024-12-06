2166e449698840aaee71494df9285abf2865caa5
87003697 <870036797@qq.com>
2024-12-06 18:52:33 +0800

    update

--- scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
+++ scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
@@ -3,12 +3,12 @@ NUM_TRAINERS=8
 HOST_NODE_ADDR=10.21.21.181
 
 
-torchrun
-    --nnodes=$1
-    --nproc-per-node=$NUM_TRAINERS
-    --max-restarts=3
-    --node_rank=$2
-    --master_port=12345
+torchrun \
+    --nnodes=$1 \
+    --nproc-per-node=$NUM_TRAINERS \
+    --max-restarts=3 \
+    --node_rank=$2 \
+    --master_port=12345 \
     python launch.py \
         --config configs/group_13/DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50.yaml \
         --train \
