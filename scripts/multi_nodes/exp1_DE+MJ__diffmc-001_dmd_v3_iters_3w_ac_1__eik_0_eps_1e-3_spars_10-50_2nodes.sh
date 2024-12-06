211c4fc571f4e52ba7d6f99a8bfec4293cb74726
87003697 <870036797@qq.com>
2024-12-06 21:57:49 +0800

    update

--- scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
+++ scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
@@ -1,25 +1,22 @@
 
-NUM_TRAINERS=8
 HOST_NODE_ADDR=10.21.21.181
 
 port=$1
 num_nodes=$2
 node_rank=$3
+nproc_per_node=8
 
-# Calculate the total number of GPUs
-total_gpus=$((nnodes * nproc_per_node))
-
-# Generate a comma-separated list of GPU indices starting from 0
-gpus=$(seq -s, 0 $((total_gpus - 1)))
+# Calculate the total number of GPUs (for reference)
+total_gpus=$((num_nodes * nproc_per_node))
 
 # Export the CUDA_VISIBLE_DEVICES environment variable
-export CUDA_VISIBLE_DEVICES=$gpus
+export CUDA_VISIBLE_DEVICES=$(seq -s, 0 $((total_gpus)))
 
 # # count the number of GPUs given the nnodes and nproc-per-node
 # export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
 torchrun \
     --nnodes=$num_nodes \
-    --nproc-per-node=$NUM_TRAINERS \
+    --nproc-per-node=$nproc_per_node \
     --max-restarts=1 \
     --node_rank=$node_rank \
     --master_port=$port \
