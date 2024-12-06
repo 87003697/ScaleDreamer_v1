d4add036eeed5a3dc7fa9a0e5c13a42d43171272
87003697 <870036797@qq.com>
2024-12-06 20:11:21 +0800

    update

--- scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
+++ scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
@@ -13,6 +13,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 torchrun \
     launch.py \
         --config configs/group_13/DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50.yaml \
         --train \
+        trainer.num_nodes=$2 \
         data.prompt_library="DALLE_Midjourney_1313928_prompt_library" \
         data.condition_processor.cache_dir="/media/test/.threestudio_cache/text_embeddings_DALLE_Midjourney" \
         data.guidance_processor.cache_dir="/media/test/.threestudio_cache/text_embeddings_DALLE_Midjourney" 
