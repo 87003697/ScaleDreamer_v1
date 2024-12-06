05b583369963376930c5a50bb95a96c81afb53d7
87003697 <870036797@qq.com>
2024-12-06 23:10:47 +0800

    update

--- scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
+++ scripts/multi_nodes/exp1_DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50_2nodes.sh
@@ -25,6 +25,7 @@ torchrun \
         --config configs/group_13/DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50.yaml \
         --train \
         trainer.num_nodes=$2 \
+        trainer.val_check_interval=10 \
         data.prompt_library="DALLE_Midjourney_1313928_prompt_library" \
         data.condition_processor.cache_dir="/dfs/ai-storage/Scaledreamer/.threestudio_cache/text_embeddings_DALLE_Midjourney" \
         data.guidance_processor.cache_dir="/dfs/ai-storage/Scaledreamer/.threestudio_cache/text_embeddings_DALLE_Midjourney" 
