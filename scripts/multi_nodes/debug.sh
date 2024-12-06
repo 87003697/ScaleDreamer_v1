45830384abaab99b56e49abe9a90eec2e6c155cc
87003697 <870036797@qq.com>
2024-12-06 20:16:05 +0800

    update

--- scripts/multi_nodes/debug.sh
+++ scripts/multi_nodes/debug.sh
@@ -1,8 +1,8 @@
 NUM_TRAINERS=8
 
 torchrun \
-    --nnodes=1 \
     --standalone \
+    --nnodes=1 \
     --nproc-per-node=$NUM_TRAINERS \
     launch.py \
         --config configs/group_13/DE+MJ__diffmc-001_dmd_v3_iters_3w_ac_1__eik_0_eps_1e-3_spars_10-50.yaml \
