5f6ba4be369a1b7664c4dc3530489e14006d0234
theEricMa <zm2354@columbia.edu>
2025-02-12 04:57:41 +0000

    update

--- scripts/to_release_v1/exp5_3DTopia__ns-200-p__sd-7.5-v1_mv-20-v1_rd-20-v1__eik_1-0_spars_1-0_eps_01_iters-4w_acc-2.sh
+++ scripts/to_release_v1/exp5_3DTopia__ns-200-p__sd-7.5-v1_mv-20-v1_rd-20-v1__eik_1-0_spars_1-0_eps_01_iters-4w_acc-2.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python launch.py \
-    --config configs/to_release_1/3DTopia__ns-100-p__sd-7.5-v1_mv-20-v1_rd-20-v1__eik_1-0_spars_1-0_eps_01_iters-2w_acc-1.yaml \
+    --config configs/to_release_1/3DTopia__ns-200-p__sd-7.5-v1_mv-20-v1_rd-20-v1__eik_1-0_spars_1-0_eps_01_iters-4w_acc-2.yaml \
     --train \
     data.prompt_library="DALLE_Midjourney_1313928_prompt_library" \
     data.condition_processor.cache_dir=".threestudio_cache/text_embeddings_DALLE_Midjourney" \
