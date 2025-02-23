a7c85b0ba80fbbadc4d1ff20f06a85d72a7ca623
87003697 <870036797@qq.com>
2025-02-23 13:28:49 +0800

    update

--- scripts/prepare_image/DE+MJ.sh
+++ scripts/prepare_image/DE+MJ.sh
@@ -7,5 +7,5 @@ python load/sdxl_animation_img_generation_v2.py \
     --prompt_libary load/DALLE_Midjourney_1313928_prompt_library.json \
     --group_total $group_total \
     --group_index $group_index \
-    --save_dir ./datasets_real/Images_3DStyle_3DTopia \
+    --save_dir ./datasets_real/Images_3DStyle_DE+MJ \
     --num_repeat 2
